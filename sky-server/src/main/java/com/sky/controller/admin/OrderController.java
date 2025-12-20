654a3229eacbb98104ffb5fff17a1c327eb17754
Jack <2484738844hhj@gmail.com>
2025-12-20 21:23:30 +0800

    营业额统计业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -13,6 +13,7 @@ import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequestMapping("/admin/order/")
 public class OrderController {
+    @Autowired
     private OrderService orderService;
 
     /**
